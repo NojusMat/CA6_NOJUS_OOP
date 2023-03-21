e368824e7dcba6f1888c9a4fe71483a2086a4859
NojusMat <d00249674@student.dkit.ie>
2023-03-21 21:50:25 +0000

    Game class added

--- src/main/java/org/example/DAOs/PlayerDaoInterface.java
+++ src/main/java/org/example/DAOs/PlayerDaoInterface.java
@@ -1,38 +1,20 @@
-package com.dkit.oop.sd2.DAOs;
+package org.example.DAOs;
 
-/** OOP Feb 2022
- * UserDaoInterface
- *
- * Declares the methods that all UserDAO types must implement,
- * be they MySql User DAOs or Oracle User DAOs etc...
- *
- * Classes from the Business Layer (users of this DAO interface)
- * should use reference variables of this interface type to avoid
- * dependencies on the underlying concrete classes (e.g. MySqlUserDao).
- *
- * More sophisticated implementations will use a factory
- * method to instantiate the appropriate DAO concrete classes
- * by reading database configuration information from a
- * configuration file (that can be changed without altering source code)
- *
- * Interfaces are also useful when testing, as concrete classes
- * can be replaced by mock DAO objects.
- */
 
-import com.dkit.oop.sd2.DTOs.User;
-import com.dkit.oop.sd2.Exceptions.DaoException;
+import org.example.DTOs.Player;
+import org.example.Exceptions.DaoException;
 import java.util.List;
 
-public interface UserDaoInterface
+public interface PlayerDaoInterface
 {
-    public List<User> findAllUsers() throws DaoException;
-
-    public void addUser()throws DaoException;
-
-    public User updatePassword(String username, String password)throws DaoException;
-    public List<User> findAllUsersLastNameContains( String lastName) throws DaoException;
-
-    public User findUserByUsernamePassword(String username, String password) throws DaoException;
+//    public List<User> findAllUsers() throws DaoException;
+//
+//    public void addUser()throws DaoException;
+//
+//    public User updatePassword(String username, String password)throws DaoException;
+//    public List<User> findAllUsersLastNameContains( String lastName) throws DaoException;
+//
+//    public User findUserByUsernamePassword(String username, String password) throws DaoException;
 
 }
 
--- src/main/java/org/example/DTOs/Player.java
+++ src/main/java/org/example/DTOs/Player.java
@@ -1,19 +1,111 @@
 package org.example.DTOs;
 
-          id INT PRIMARY KEY,
-          name VARCHAR(50),
-          team VARCHAR(50),
-          height_in_cm INT,
-          weight_in_kg INT,
-          points_per_game FLOAT
 
-
-public class Players {
+public class Player {
 
     private int id;
-    private String firstname;
-    private String lastname;
-    private int height_in_cm;
-    private int weight_in_kg ;
-    private double points_per_game;
+    private String firstName;
+    private String lastName;
+    private String team;
+    private double height_in_Cm;
+    private float points_Per_Game;
+    private int weight_in_Kg ;
+
+
+    public Player(int id, String firstName, String lastName,String team, double height_in_Cm, int weight_in_Kg, float points_Per_Game)
+    {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.team=team;
+        this.height_in_Cm = height_in_Cm;
+        this.weight_in_Kg = weight_in_Kg;
+        this.points_Per_Game = points_Per_Game;
+    }
+//    public Player(String firstName, String lastName, double  height_in_Cm, int  weight_in_Kg, float points_Per_Game)
+//    {
+//        this.id = 0;
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//        this.height_in_Cm = height_in_Cm;
+//        this.weight_in_Kg = weight_in_Kg;
+//        this.points_Per_Game = points_Per_Game;
+//    }
+//    public Player()
+//    {
+//    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+
+    public String getTeam()
+    {
+        return team;
+    }
+
+    public void setTeam(String team)
+    {
+        this.team = team;
+    }
+
+
+    public double getHeight_in_Cm()
+    {
+        return height_in_Cm;
+    }
+
+    public void setHeight_in_Cm(double height_in_Cm)
+    {
+        this.height_in_Cm = height_in_Cm;
+    }
+
+
+    public float getPoints_Per_Game() {return points_Per_Game;}
+
+    public void setPoints_Per_Game(float points_Per_Game) {this.points_Per_Game = points_Per_Game;}
+
+
+    public int getWeight_in_Kg() {return weight_in_Kg;}
+
+    public void setWeight_in_Kg(int weight_in_Kg) {this.weight_in_Kg = weight_in_Kg;}
+
+
+
+    @Override
+    public String toString()
+    {
+        return "Player{" + "ID=" + id + ", First Name=" + firstName + ", Last Name=" +", Team=" + team +
+                lastName + ", Height in Cm=" + height_in_Cm +", Weight in  Kg=" +weight_in_Kg+ ", Points Per Game=" + points_Per_Game+'}';
+    }
+
 }
