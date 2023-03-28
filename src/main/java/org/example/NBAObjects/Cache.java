ca85fd6659fef3434287e8c3a245472cb792468e
NojusMat <d00249674@student.dkit.ie>
2023-03-28 21:45:02 +0100

    Feature 6 6.2 created Cache Class

---
+++ src/main/java/org/example/NBAObjects/Cache.java
@@ -0,0 +1,17 @@
+package org.example.NBAObjects;
+
+import org.example.DTOs.Player;
+import org.example.IFilter;
+
+import java.util.HashSet;
+
+public class Cache {
+    private HashSet<Integer> id;
+
+    public Cache(){
+        this.id=new HashSet<Integer>();
+    }
+
+
+}
+
