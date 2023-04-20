5b3fed86e8e06a61667d72a8eb18287590c1c27f
NojusMat <d00249674@student.dkit.ie>
2023-04-20 21:57:22 +0100

    testing for player DTO

--- src/test/java/org/example/DTOsTest/PlayerTest.java
+++ src/test/java/org/example/DTOsTest/PlayerTest.java
@@ -1,74 +1,118 @@
 package org.example.DTOsTest;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
 
-class PlayerTest {
+import java.util.HashSet;
+import java.util.Set;
 
-    @Test
-    void getId() {
+import org.junit.Before;
+import org.junit.Test;
+
+public class PlayerTest {
+
+    private Player player;
+
+    @Before
+    public void setUp() {
+        Set<Integer> cache = new HashSet<>();
+        cache.add(1);
+        cache.add(2);
+        player = new Player(1, "John", "Doe", "Team A", 185.4, 80, 20.5f);
+
     }
 
     @Test
-    void getFirstName() {
+    public void testGetId() {
+        assertEquals(1, player.getId());
     }
 
     @Test
-    void setFirstName() {
+    public void testGetFirstName() {
+        assertEquals("John", player.getFirstName());
     }
 
     @Test
-    void getLastName() {
+    public void testGetLastName() {
+        assertEquals("Doe", player.getLastName());
     }
 
     @Test
-    void setLastName() {
+    public void testGetTeam() {
+        assertEquals("Team A", player.getTeam());
     }
 
     @Test
-    void getTeam() {
+    public void testGetHeight_in_Cm() {
+        assertEquals(185.4, player.getHeight_in_Cm(), 0.001);
     }
 
     @Test
-    void setTeam() {
+    public void testGetWeight_in_Kg() {
+        assertEquals(80, player.getWeight_in_Kg());
     }
 
     @Test
-    void getHeight_in_Cm() {
+    public void testGetPoints_Per_Game() {
+        assertEquals(20.5f, player.getPoints_Per_Game(), 0.001);
+    }
+
+//    @Test
+//    public void testSetId() {
+//        player.setId(2);
+//        assertEquals(2, player.getId());
+//    }
+
+    @Test
+    public void testSetFirstName() {
+        player.setFirstName("Jane");
+        assertEquals("Jane", player.getFirstName());
     }
 
     @Test
-    void setHeight_in_Cm() {
+    public void testSetLastName() {
+        player.setLastName("Smith");
+        assertEquals("Smith", player.getLastName());
     }
 
     @Test
-    void getWeight_in_Kg() {
+    public void testSetTeam() {
+        player.setTeam("Team B");
+        assertEquals("Team B", player.getTeam());
     }
 
     @Test
-    void setWeight_in_Kg() {
+    public void testSetHeight_in_Cm() {
+        player.setHeight_in_Cm(180.0);
+        assertEquals(180.0, player.getHeight_in_Cm(), 0.001);
     }
 
     @Test
-    void getPoints_Per_Game() {
+    public void testSetWeight_in_Kg() {
+        player.setWeight_in_Kg(75);
+        assertEquals(75, player.getWeight_in_Kg());
     }
 
     @Test
-    void setPoints_Per_Game() {
+    public void testSetPoints_Per_Game() {
+        player.setPoints_Per_Game(15.5f);
+        assertEquals(15.5f, player.getPoints_Per_Game(), 0.001);
     }
 
     @Test
-    void displayPlayer() {
+    public void testCompareTo() {
+        Player tallerPlayer = new Player(2, "Mike", "Smith", "Team A", 190.0, 85, 18.5f);
+        assertEquals(-1, player.compareTo(tallerPlayer));
     }
 
     @Test
-    void testToString() {
+    public void testHashCode() {
+        Player playerWithSameAttributes = new Player(1, "John", "Doe", "Team A", 185.4, 80, 20.5f);
+        assertEquals(player.hashCode(), playerWithSameAttributes.hashCode());
     }
 
-    @Test
-    void testHashCode() {
-    }
-
-    @Test
-    void compareTo() {
-    }
+//    @Test
+//    public void testToString() {
+//        String expected = "{ID=1,| First Name=John,| Last Name=Doe,| Team=Team A,| Height in Cm=185.4,| Weight in  Kg=80,| Points Per Game=20.5}";
+//        assertEquals
+//    }
 }
